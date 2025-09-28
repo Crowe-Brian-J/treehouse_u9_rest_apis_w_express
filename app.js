268ae5aba9f47fd84075c62f44dc260666f07cd3
Brian Crowe <brian.j.crowe@protonmail.com>
2025-09-27 23:02:47 -0400

    plan REST api routes

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 const express = require('express')
 const app = express()
 
+// First GET Method
 app.get('/greetings', (req, res) => {
   res.json({ greeting: 'Hello World!' })
 })
+
+// Send a GET request to /quotes to READ a list of quotes
+// Send a GET request to /quotes/:id to READ (view) a quote
+// Send a POST request to /quotes CREATE a new quote
+// Send a PUT request to /quotes/:id UPDATE (edit) a quote
+// Send a DELETE request /quotes/:id to DELETE a quote
+// Send a GET request to /quotes/quote/random to READ a random quote
 
 app.listen(3000, () => console.log('Quote API listening on port 3000!'))
